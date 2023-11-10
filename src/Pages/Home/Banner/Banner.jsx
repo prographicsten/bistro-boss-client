d24c0f21223280ba14ffa5e2209b4403bfd294ca
prographicsten <prographicsten@gmail.com>
2023-11-10 23:40:39 +0600

    correct location files setup

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,12 +3,12 @@ import { Carousel } from 'react-responsive-carousel';
 
 
 // banner images call
-import img1 from '../../assets/home/01.jpg'
-import img2 from '../../assets/home/02.jpg'
-import img3 from '../../assets/home/03.png'
-import img4 from '../../assets/home/04.jpg'
-import img5 from '../../assets/home/05.png'
-import img6 from '../../assets/home/banner.jpg'
+import img1 from "../../../assets/home/01.jpg"
+import img2 from "../../../assets/home/02.jpg"
+import img3 from "../../../assets/home/03.png"
+import img4 from "../../../assets/home/04.jpg"
+import img5 from "../../../assets/home/05.png"
+import img6 from "../../../assets/home/banner.jpg"
 
 const Banner = () => {
     return (
